404aea9b95550efe988278f677c1057699ea776c
shivajiraje <vinod@archesoftronix.com>
2021-04-14 16:37:50 +0530

    changes

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
     baseUrl: 'http://www.apiats.somee.com/api/',
     //baseUrl: 'http://192.168.0.11/api/'
     //baseUrl: 'http://192.168.1.200:5789/api/'
-    crmUrl :"http://e74da64330d7.ngrok.io/"
+    crmUrl :"http://74d397dcff3d.ngrok.io/"
     
 }
