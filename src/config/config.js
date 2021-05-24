13aff59c3811d1bdf0c57a9538ab540c33088a53
shivajiraje <vinod@archesoftronix.com>
2021-05-24 10:56:19 +0530

    ulr archesoft

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     //baseUrl: 'http://webapplication3-prod.eu-west-1.elasticbeanstalk.com/api/',
-    baseUrl: 'http://apiats.somee.com/api/',
+    //baseUrl: 'http://apiats.somee.com/api/',
+    baseUrl:'https://clsapi.archesoftronix.in/api/',
     //baseUrl: 'http://192.168.0.11/api/'
     //baseUrl: 'http://192.168.1.200:5789/api/'
     crmUrl :"http://57157a36e723.ngrok.io/"
