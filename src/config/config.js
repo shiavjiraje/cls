2a04c3a6286b63e6e31ba6bac3276f5d0b768fa2
shivajiraje <vinod@archesoftronix.com>
2021-08-27 16:56:49 +0530

    api change

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     //baseUrl: 'http://webapplication3-prod.eu-west-1.elasticbeanstalk.com/api/',
    // baseUrl: 'http://apiats.somee.com/api/',
-    baseUrl:'https://clsapi.archesoftronix.in/api/',
+   // baseUrl:'https://clsapi.archesoftronix.in/api/',
+   baseUrl:'https://clscsapi.clscs.ie/api/',
     //baseUrl: 'http://192.168.0.11/api/'
     //baseUrl: 'http://192.168.1.200:5789/api/'
     crmUrl :"http://57157a36e723.ngrok.io/"
